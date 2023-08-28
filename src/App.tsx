69353048703e8a32bdfbb3429790b851149e272b
TheAppleFreak <theapplefreak@gmail.com>
2023-08-28 09:51:29 +0000

    Began setting up Tauri rewrite

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Component } from "preact";
+// import { invoke } from "@tauri-apps/api/tauri";
+import "./App.css";
+
+class App extends Component {
+	render() {
+		return (
+			<main>
+                <p>hi</p>
+			</main>
+		);
+	}
+}
+
+export default App;
